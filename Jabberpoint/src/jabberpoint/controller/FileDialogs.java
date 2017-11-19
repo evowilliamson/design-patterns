5b68d94dcb69014761f62fa22589a3aa2ab10a26
rpottgens <31620822+rpottgens@users.noreply.github.com>
2017-11-19 15:47:09 +0100

    Adding XML filter in FileChooser

--- Jabberpoint/src/jabberpoint/controller/FileDialogs.java
+++ Jabberpoint/src/jabberpoint/controller/FileDialogs.java
@@ -4,13 +4,17 @@ import java.awt.Frame;
 import java.io.File;
 
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import jabberpoint.model.action.ActionFactory;
 
 public class FileDialogs {
 	public static void loadFile(Frame frame){
 		System.out.println("loadFile chosen.");
-        JFileChooser fc = new JFileChooser();
+		JFileChooser fc = new JFileChooser();
+		FileFilter filter = new FileNameExtensionFilter("XML presentations", "xml");
+		fc.addChoosableFileFilter(filter);
 		int r = fc.showOpenDialog(frame);
 		if (r == JFileChooser.APPROVE_OPTION) {
 			System.out.println("Opening file...");
@@ -22,6 +26,8 @@ public class FileDialogs {
 	public static void saveFile(Frame frame){
 		System.out.println("saveFile chosen.");
         JFileChooser fc = new JFileChooser();
+        FileFilter filter = new FileNameExtensionFilter("XML presentations", "xml");
+        fc.addChoosableFileFilter(filter);
 		int r = fc.showSaveDialog(frame);
 		if (r == JFileChooser.APPROVE_OPTION) {
 			System.out.println("Saving file...");
