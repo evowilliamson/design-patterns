16aca61b96013f8cfed0008297361e11fdb8e984
Ivo Willemsen <ivo_willemsen@hotmail.com>
2017-11-19 12:16:19 +0100

    adding comments

--- Jabberpoint/src/jabberpoint/model/slide/SlideFactory.java
+++ Jabberpoint/src/jabberpoint/model/slide/SlideFactory.java
@@ -1,7 +1,18 @@
 package jabberpoint.model.slide;
 
+/**
+ * Class that creates a new slide
+ * @author Randy Pottgens
+ */
 public class SlideFactory {
+
+	/**
+	 * This method creates a new slide
+	 * @param title the title of the slide
+	 * @return the Slide object
+	 */
 	public static Slide createSlide(String title){
 		return new Slide(title);
 	}
+
 }
--- Jabberpoint/src/jabberpoint/view/SlideViewerFactory.java
+++ Jabberpoint/src/jabberpoint/view/SlideViewerFactory.java
@@ -2,14 +2,17 @@ package jabberpoint.view;
 
 import jabberpoint.controller.MouseController;
 
+/**
+ * Class that creates View objects
+ * @author Randy Pottgens
+ */
 public class SlideViewerFactory {
 	public static SlideViewerFrame createSlideViewerFrame(String title){
 		return new SlideViewerFrame(title);
 	}
-	/*public static SlideViewerComponent getSlideViewerComponent(){
-		return new SlideViewerComponent();
-	}*/
+
 	public static SlideViewerComponent createSlideViewerComponent(MouseController mouseController){
 		return new SlideViewerComponent(mouseController);
 	}
+
 }
